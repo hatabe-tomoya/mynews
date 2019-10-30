908840948ad966c215750fd6cb2ea194eb8d0d51
”hatabe_tomoya” <arkelle1559@gmail.com>
2019-10-30 02:51:12 +0000

    PHP/Laravel 10 fix

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>Myニュース編集画面</title>
+        <title>Myプロフィール編集画面</title>
     </head>
     <body>
-        <h1>Myニュース編集画面</h1>
+        <h1>Myプロフィール編集画面</h1>
     </body>
 </html>
