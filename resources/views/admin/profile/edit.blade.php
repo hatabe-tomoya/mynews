ffc90b466cb64ebe5c5559fc67a0b23bf0371630
”hatabe_tomoya” <arkelle1559@gmail.com>
2019-11-11 06:29:17 +0000

    １６章課題

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,7 +1,6 @@
-{{-- 課題12-1 --}}
+{{-- 課題16-3 --}}
 
 @extends('layouts.profile')
-
 @section('title', 'Myプロフィール')
 
 @section('content')
@@ -9,6 +8,47 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール編集画面</h2>
+                <form action ="{{ action('Admin\ProfileController@update') }}" method="post">
+                    @if(count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row" >
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="radio"   name="gender" value="male" <?php print ((old('gender', $profile_form->gender)== "male") ? " checked" : ""); ?>>男性
+                            <input type="radio"   name="gender" value="female"<?php print ((old('gender', $profile_form->gender)== "female") ? " checked" : ""); ?>>女性
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea type="text" class="form-control" name="introduction" row="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
